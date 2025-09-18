4854d9a01d86fe9c6c6008398f504af191337015
EmillySatiro <sousasatiroclaraemilly@gmail.com>
2025-09-18 10:17:12 -0300

    feat: implementar servidor escravo para contagem de letras e números

---
+++ src/server/slave/main.cpp
@@ -0,0 +1,11 @@
+#include "server.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Uso: " << argv[0] << " [letras|numeros]\n";
+        return 1;
+    }
+    run_slave_server(argv[1]);
+    return 0;
+}
